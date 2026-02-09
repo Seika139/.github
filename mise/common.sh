119b4f93b9427b28ebfb270a357bdbc95e37669e
Seika139 <tcoanzu@gmail.com>
2026-02-10 02:10:18 +0900

    より堅牢に .python-version ファイルを読み取るように変更しました。

--- mise/common.sh
+++ mise/common.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 get_python_version() {
-  if [ -f .python-version ]; then
-    cat .python-version | xargs
+  if [ -s .python-version ]; then
+    xargs <.python-version
   else
-    echo 'Error: .python-version not found'
+    echo 'Error: .python-version not found or is empty' >&2
     exit 1
   fi
 }
